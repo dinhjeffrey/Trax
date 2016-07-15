6ef12c3fa4615d335636468ea14864da3dfcb803
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-07-14 18:48:28 -0700

    displaying map with pin locations working

---
+++ Trax/GPXViewController.swift
@@ -0,0 +1,77 @@
+//
+//  ViewController.swift
+//  Trax
+//
+//  Created by jeffrey dinh on 7/14/16.
+//  Copyright © 2016 jeffrey dinh. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class GPXViewController: UIViewController, MKMapViewDelegate {
+    
+    var gpxURL: NSURL? {
+        didSet {
+            clearWaypoints()
+            if let url = gpxURL {
+                GPX.parse(url) { gpx in
+                    if gpx != nil {
+                        self.addWaypoints(gpx!.waypoints)
+                    }
+                }
+            }
+        }
+    }
+    
+    private func clearWaypoints() {
+        mapView?.removeAnnotations(mapView.annotations)
+    }
+    
+    private func addWaypoints(waypoints: [GPX.Waypoint]) {
+        mapView?.addAnnotations(waypoints)
+        mapView?.showAnnotations(waypoints, animated: true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        gpxURL = NSURL(string: "http://cs193p.stanford.edu/Vacation.gpx")
+    }
+    
+    @IBOutlet weak var mapView: MKMapView! {
+        didSet {
+            mapView.mapType = .Satellite
+            mapView.delegate = self
+        }
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
---
+++ Trax/MKGPX.swift
@@ -0,0 +1,60 @@
+//
+//  MKGPX.swift
+//  Trax
+//
+//  Created by jeffrey dinh on 7/14/16.
+//  Copyright © 2016 jeffrey dinh. All rights reserved.
+//
+
+import MapKit
+
+extension GPX.Waypoint: MKAnnotation {
+    var coordinate: CLLocationCoordinate2D {
+        return CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+    
+    var title: String? { return name }
+    
+    var subtitle: String? { return info }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
